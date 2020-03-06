65eb809e8a0ab8999a792e2166b624d0a7f1d124
zsiskos <43017972+zsiskos@users.noreply.github.com>
2020-03-06 16:21:49 -0500

    all the files

---
+++ js/main.js
@@ -0,0 +1,36 @@
+//variables
+let counter = 0
+
+//references
+let counterEl = document.getElementById('counter')
+let inputVal = document.getElementById("input").value = 1
+let upBtnEl = document.getElementById('upBtn')
+let downBtnEl = document.getElementById('downBtn')
+
+//event listeners
+upBtnEl.addEventListener('click', addValue)
+downBtnEl.addEventListener('click', minusValue)
+
+//functions
+function addValue(){
+    let input = parseInt(document.getElementById("input").value)
+    counter = counter + input
+    counterEl.innerHTML = counter
+    render()
+}
+
+function minusValue() {
+    let input = parseInt(document.getElementById("input").value)
+    counter = counter - input
+    counterEl.innerHTML = counter
+    render()
+}
+
+function render() {
+    if (counter > 0) {
+        counterEl.style.color = 'black'       
+    } else if (counter < 0) {
+        counterEl.style.color = 'red'       
+    }   
+}
+
